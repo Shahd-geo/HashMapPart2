a6e2177c19a2187c17c0d0935e3a71e380c1384b
Shahd <shahad9mubarak@gmail.com>
2026-04-19 16:05:19 +0400

    inset set

---
+++ src/TASK1.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TASK1 {
+    static void main(String[] args) {
+        Map<String,Integer> wordHasgMap=new HashMap<>();
+        for (String word :stringes){
+            ma
+        }
+    }
+}
