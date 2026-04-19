1e1d42d0eaea4e60fcb2aa9bab58d1037ac3ab71
Shahd <shahad9mubarak@gmail.com>
2026-04-19 16:37:27 +0400

    creat HashMap and set

--- src/TASK3.java
+++ src/TASK3.java
@@ -3,14 +3,17 @@ import java.util.Map;
 
 public class TASK3 {
     static void main(String[] args) {
-        String[] strings = {"this", "and", "that", "and"};
-        System.out.println(wordLen(strings));
+        String[] strings = {"code", "bug"};
+        System.out.println(pairs(strings));
 
     }
     public static Map<String, String> pairs(String[] strings) {
         Map<String, String> map = new HashMap<>();
         for (String word : strings) {
-            map.put(word.charAt(i), word.charAt(word.length()-1));
+            String key = word.charAt(0) + "";
+            String value = word.charAt(word.length() - 1) + "";
+
+            map.put(key, value);
         }
 
         return map;
