a11db5fada606e9ebb9d16ed42e9eee90366e6cb
Shahd <shahad9mubarak@gmail.com>
2026-04-20 10:54:08 +0400

    task5:Return a Map<String, String>  frist  char

--- src/TASK5.java
+++ src/TASK5.java
@@ -3,6 +3,8 @@ import java.util.Map;
 
 public class TASK5 {
     static void main(String[] args) {
+        String[]strings={"salt", "tea", "soda", "toast"};
+        System.out.println(fristChar(strings));
 
     }
     public static Map<String,String>fristChar(String[]strings){
@@ -10,8 +12,12 @@ public class TASK5 {
         for (String WORD:strings){
             String KEY=WORD.substring(0,1);
             if (map.containsKey(KEY)){
+                map.put(KEY,map.get(KEY)+WORD);
 
+            }else {
+                map.put(KEY,WORD);
             }
         }
+        return map;
     }
 }
