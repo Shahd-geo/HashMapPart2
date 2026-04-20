8eb7cf292065aed80f672b6998d8b9410746e579
Shahd <shahad9mubarak@gmail.com>
2026-04-20 11:34:09 +0400

    TASK 7 :Return a Map<String, Boolean>wordMultiple

--- src/TASK7.java
+++ src/TASK7.java
@@ -3,12 +3,22 @@ import java.util.Map;
 
 public class TASK7 {
     static void main(String[] args) {
-
+       // String[] strings={"a", "b", "a", "c", "b"};
+        String[] strings= {"c", "b", "a"};
+        System.out.println(wordMultiple(strings));
     }
-    public static Map<String,Boolean>wordMultiple(String[] strings){
-        Map<String,Boolean>map=new HashMap<>();
-        for (String WORD:strings){
+
+    public static Map<String, Boolean> wordMultiple(String[] strings) {
+        Map<String, Boolean> map = new HashMap<>();
+        for (String WORD : strings) {
+            if (!map.containsKey(WORD)) {
+                map.put(WORD, false);
+            } else {
+                map.put(WORD, true);
+            }
 
         }
+        return map;
     }
 }
+
