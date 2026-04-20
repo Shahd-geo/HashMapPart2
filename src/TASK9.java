627680fe094923046042dbe6211f0fb6572b5567
Shahd <shahad9mubarak@gmail.com>
2026-04-20 14:29:42 +0400

    TASK9 hashmap

--- src/TASK9.java
+++ src/TASK9.java
@@ -1,6 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TASK9 {
     static void main(String[] args) {
 
      }
-     public static String[]
+     public static String[]firstSwap(String[] strings){
+         Map<String,Integer>map=new HashMap<>()
+     }
 }
