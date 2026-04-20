15941ef0a4b6297b1135607f50c85894cd8940ea
Shahd <shahad9mubarak@gmail.com>
2026-04-20 11:30:16 +0400

    building hashmap

--- src/TASK7.java
+++ src/TASK7.java
@@ -1,5 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TASK7 {
     static void main(String[] args) {
 
+    }
+    public static Map<String,Boolean>wordMultiple(String[] strings){
+        Map<String,Boolean>map=new HashMap<>();
+        for (String WORD:strings){
+
+        }
     }
 }
