1bf6a74cf6ae0e8366cfc269f9636ae1636d92cb
Shahd <shahad9mubarak@gmail.com>
2026-04-20 11:50:41 +0400

    DOING SWAP

--- src/TASK8.java
+++ src/TASK8.java
@@ -3,12 +3,25 @@ import java.util.Map;
 
 public class TASK8 {
     static void main(String[] args) {
+        String[]strings=
 
     }
     public static String[] allSwap(String[]strings){
         Map<String,Integer>map=new HashMap<>();
         for (int i = 0 ; i <strings.length;i++){
+            String word=strings[i];
+            String key = word.substring(0,1);
+            if (map.containsKey(key)){
+                int index =map.get(key);
+                String swapp=strings[i];
+                strings[i]=strings[index];
+                strings[index]=swapp;
+                map.remove(key);
+            }else{
+                map.put(key,i);
+            }
 
         }
+        return strings;
     }
 }
