03c6ceec91f24a793996452ce18d5233df0a2a01
Shahd <shahad9mubarak@gmail.com>
2026-04-20 11:06:04 +0400

    BUILD HASHMAP

--- src/TASK6.java
+++ src/TASK6.java
@@ -1,5 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TASK6 {
     static void main(String[] args) {
+        String[] strings={"a", "b", "a"};
+        System.out.println();
 
+    }
+    public static String wordAppend(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        String result = "";
+
+        for (String word : strings) {
+
+            map.put(word, map.getOrDefault(word, 0) + 1);
+
+
+            if (map.get(word) % 2 == 0) {
+                result += word;
+            }
+        }
+
+        return result;
     }
 }
