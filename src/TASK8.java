fe81b3cf59fde1a3cf202a378090e755767fa195
Shahd <shahad9mubarak@gmail.com>
2026-04-20 11:44:35 +0400

    building HashMap

--- src/TASK8.java
+++ src/TASK8.java
@@ -1,5 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TASK8 {
     static void main(String[] args) {
 
+    }
+    public static String[] allSwap(String[]strings){
+        Map<String,Integer>map=new HashMap<>();
+        for (int i = 0 ; i <strings.length;i++){
+
+        }
     }
 }
