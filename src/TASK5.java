302f4ee1aa0721364fc4f916905f8b593d0ce2a0
Shahd <shahad9mubarak@gmail.com>
2026-04-20 10:50:54 +0400

    building if statment

--- src/TASK5.java
+++ src/TASK5.java
@@ -1,8 +1,17 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class TASK5 {
     static void main(String[] args) {
 
     }
-    public static Map<String,String>
+    public static Map<String,String>fristChar(String[]strings){
+        Map<String,String>map=new HashMap<>();
+        for (String WORD:strings){
+            String KEY=WORD.substring(0,1);
+            if (map.containsKey(KEY)){
+
+            }
+        }
+    }
 }
